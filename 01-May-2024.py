052c0947fc5c58e0edb5ea0424b064936f1c1ad1
sujannani <sujan.kommalapati@gmail.com>
2024-05-13 16:57:52 +0530

    01-05-2024

---
+++ 01-May-2024.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        if ch not in word:
+            return word
+        res=""
+        for i in range(len(word)):
+            if word[i]==ch:
+                res=word[i]+res
+                break
+            else:
+                res=word[i]+res
+        res+=word[i+1:]
+        return res
+            
