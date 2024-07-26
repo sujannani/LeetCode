c42d40e49160df8ca8ee93b0a1ac00aee61e1aff
sujannani <sujan.kommalapati@gmail.com>
2024-07-26 13:18:01 +0530

    2487

---
+++ 06-May-2024.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        ma=-float('inf')
+        sta=[]
+        temp=head
+        while temp:
+            if len(sta)==0:
+                sta.append(temp.val)
+                ma=sta[0]
+            elif temp.val>ma:
+                sta=[temp.val]
+                ma=sta[0]
+            elif temp.val>sta[-1]:
+                while temp.val>sta[-1]:
+                    sta.pop(-1)
+                sta.append(temp.val)
+            else:
+                sta.append(temp.val)
+            temp=temp.next
+        r=ListNode(0)
+        t=r
+        for i in sta:
+            t.next=ListNode(i)
+            t=t.next
+        return r.next
+            
