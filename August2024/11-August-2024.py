e4ab1b460425097510186b0e6b18c6d0da1758b6
sujannani <sujan.kommalapati@gmail.com>
2024-08-12 22:35:19 +0530

    703

---
+++ August2024/11-August-2024.py
@@ -0,0 +1,9 @@
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.nums=nums
+        self.k=k
+
+    def add(self, val: int) -> int:
+        self.nums.append(val)
+        self.nums.sort()
+        return self.nums[-self.k]
